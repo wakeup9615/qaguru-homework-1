225166df0023f8a61f4c16c1565d53e663b7fe47
Ekaterina Astaskina <ekaterina.fedoseeva@rt.ru>
2022-05-30 21:16:57 +0300

    first commit

---
+++ src/test/java/guru/qa/FirstClass.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstClass {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 == 3);
+    }
+
+    @Test
+    void second_firstTest() {
+        Assertions.assertTrue(4 == 4);
+    }
+}
